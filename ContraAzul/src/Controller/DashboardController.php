c1b9ae1ef7f39415355a47098efd55c368019500
CarlosIglesias <carlos.iglesias@enevasys.com>
2020-09-04 17:27:48 -0500

    Hasta aqui esta entregable sin form css y sin funcionalidad de botones en sidebar

--- ContraAzul/src/Controller/DashboardController.php
+++ ContraAzul/src/Controller/DashboardController.php
@@ -8,6 +8,12 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
+
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 // use App\Entity\User;
 // use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 // use Symfony\Component\HttpFoundation\Session\SessionInterface;
@@ -27,13 +33,31 @@ class DashboardController extends AbstractController{
  * @Route("/", name="_home")
  * @Method({"GET"})
  */
-    public function home()
+    public function home(Request $request, SessionInterface $session)
     {
-        // $this->denyAccessUnlessGranted(['ROLE_USER', 'ROLE_SUPER_ADMIN', 'ROLE_ADMIN']);
+        
+        // dd(is_numeric(strpos(strtolower($_SERVER['HTTP_USER_AGENT']), "mobile")));
+        $form = $this->createFormBuilder()
+        ->add('Name',TextType::class,['label' => false,
+        'attr' => ['maxlength' => 30, 'minlength' => 1, 'placeholder' => 'Nombre *'] ])
+        ->add('Email',TextType::class,['label' => false,
+        'attr' => ['maxlength' => 30, 'minlength' => 1, 'placeholder' => 'Email *'] ])
+        ->add('Asunto',TextType::class,['label' => false,
+        'attr' => ['maxlength' => 30, 'minlength' => 1, 'placeholder' => 'Asunto '] ])
+        ->add('Description', TextareaType::class, ['label' => false,
+                                            'attr' => ['minlength' => 10, 'placeholder' => 'Descripcion '] ])
+        ->add('save',SubmitType::class,['label' => 'Submit'])
+        ->getForm();
+    
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
 
-     
-
-        return $this->render('landing.html.twig');
+        }
+        return $this->render('landing.html.twig',[
+            'form'=> $form->createView(),
+            'isMobile' => is_numeric(strpos(strtolower($_SERVER['HTTP_USER_AGENT']), "mobile"))
+        ]);
+        
     }
 }
 ?>
